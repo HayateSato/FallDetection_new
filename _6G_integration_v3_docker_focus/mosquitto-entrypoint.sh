d09f9718c4343055bcf9a65a01f474c335ba40b6
Hayate Sato <hayatesato1995@gmail.com>
2026-06-05 16:53:01 +0200

    bug unable to open passw firle for mqtt broker - client auth

--- _6G_integration_v3_docker_focus/mosquitto-entrypoint.sh
+++ _6G_integration_v3_docker_focus/mosquitto-entrypoint.sh
@@ -6,7 +6,9 @@ if [ -z "$MQTT_USERNAME" ] || [ -z "$MQTT_PASSWORD" ]; then
     exit 1
 fi
 
+rm -f /mosquitto/config/passwd
 mosquitto_passwd -c -b /mosquitto/config/passwd "$MQTT_USERNAME" "$MQTT_PASSWORD"
+chown mosquitto:mosquitto /mosquitto/config/passwd
 echo "MQTT: password file generated for user '$MQTT_USERNAME'"
 
 exec /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
